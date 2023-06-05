4f021ddc6f029d523a7832f3cec962b0c4139d2f
81903 <crayon5364.gmail.com>
2023-06-05 15:59:24 +0900

    getAllMessagesを追加

---
+++ src/controllers/lndexServlet.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+import utils.DBUtil;
+
+
+
+@WebServlet("/index")
+public class lndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    public lndexServlet() {
+        super();
+
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages", Message.class).getResultList();
+        response.getWriter().append(Integer.valueOf(messages.size()).toString());
+
+        em.close();
+
+    }
+
+}
